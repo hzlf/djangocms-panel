74709e058016f614147fc1a796fc8dd269fead72
Jonas Ohrstrom <ohrstrom@gmail.com>
2015-12-08 11:54:52 +0100

    simplified plugin for usage out of bootstrap

--- djangocms_panel/models.py
+++ djangocms_panel/models.py
@@ -3,21 +3,9 @@ from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
 
 class Panel(CMSPlugin):
-	DEFAULT = 'panel-default'
-	PRIMARY = 'panel-primary'
-	SUCCESS = 'panel-success'
-	INFO = 'panel-info'
-	WARNING = 'panel-warning'
-	DANGER = 'panel-danger'
-	CONTEXT_CHOICES = (
-		(DEFAULT, 'Default'),
-		(PRIMARY, 'Primary'),
-		(SUCCESS, 'Success'),
-		(INFO, 'Info'),
-		(WARNING, 'Warning'),
-		(DANGER, 'Danger'),
-	)
 
-	context = models.CharField(max_length=25, choices=CONTEXT_CHOICES, default=DEFAULT)
-	title = models.CharField(max_length=100, null=True, blank=True)
-	footer = models.CharField(max_length=100, null=True, blank=True)
+	extra_classes = models.CharField(max_length=100, null=True, blank=True)
+
+	def get_extra_classes(self, separator=" "):
+		return separator.join(self.extra_classes.split(','))
+
